046e093d15d7784eeecb7adce8a387425887cadf
Bruno Lima <brunoadl88@gmail.com>
2022-07-12 14:07:29 -0300

    update

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-function mandaZap (element) {
-    let tel = prompt('Para qual número deseja mandar? (11 12345-6789')
-    let message = element.firstElementChild.innerText
-    let zapLink = `https://api.whatsapp.com/send?phone=55${tel}&text=${message}`
-    window.open(zapLink)
+function mandaZap(element) {
+  const message = element.textContent;
+  const tel = prompt("Para qual número deseja mandar? (11) 12345-6789");
+  if (tel) {
+    window.open(`https://api.whatsapp.com/send?phone=55${tel}&text=${message}`);
+  }
 }
 
-function addMessage () {
-    const add = document.querySelector('#master')
-    const newMessage = add.children[0].cloneNode(true)
-    const pos = add.children.length - 1
-    add.insertBefore(newMessage, add.children[pos])
-    let message =prompt('Digite a nova mensagem')
-    add.children[pos].querySelector('p').innerText = message
+function addMessage() {
+  const message = prompt("Digite a nova mensagem");
+  if(message) {
+  const master = document.querySelector("#master");
+  const newMessage = master.children[0].cloneNode(true);
+  const pos = master.children.length - 1;
+  master.insertBefore(newMessage, master.children[pos]);
+  master.children[pos].querySelector("p").innerText = message;
+  }
 }
